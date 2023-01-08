de3fa22705865282a14c4cb0d7a257ab32dc78aa
BlazingIsFire <firemanandy1102@gmail.com>
2023-01-08 13:58:24 -0500

    Created homepage and form with search bar for application.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import './stylesheets/App.css';
+import { ReactComponent as SunSVG } from './imgs/sun.svg';
+import { ReactComponent as SearchSVG } from './imgs/search.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="app-container flex-center-all flex-column">
+      <div className="app-header flex flex-row">
+        <h2>Gitfinder</h2>
+        <span className="theme-label flex flex-align-center">Light
+          <SunSVG id="sun-svg" className="pointer"/>
+        </span>
+      </div>
+      <div className="flex-center-all">
+        <form className="app-search flex">
+          <SearchSVG id="search-svg"/>
+          <input id='github-search' type='text' placeholder="Search Github username..."/>
+          <input id='github-search-submit' type='submit' value='Search' className="pointer"/>
+        </form>
+      </div>
     </div>
+    </>
   );
 }
 
